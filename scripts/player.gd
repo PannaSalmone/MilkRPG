bf32b2b8f218458118ece38b5841de318ea87642
Panna <seanbb.3s@gmail.com>
2023-04-13 18:11:54 +0200

    1

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,52 +1,49 @@
 extends CharacterBody2D
 
+@export var walk_speed = 4.0
 const TILE_SIZE = 32
 
-var direction: Vector2 = Vector2.ZERO
-var pixels_per_second: set = _pixels_per_second_changed
+@onready var ray = $RayCast2D
 
-var _step_size: float
-func _pixels_per_second_changed(value: float) -> void:
-	pixels_per_second = value
-	_step_size = (1 / pixels_per_second)
+var initial_position = Vector2(0, 0,)
+var input_direction = Vector2(0, 0)
+var is_moving = false
+var percent_moved_to_next_tile = 0.0
 
-# Accumulates delta, aka fractions of seconds, to time movement
-var _step: float = 0
-# Count movement in distinct integer steps
-var _pixels_moved: int = 0 
+#Called when the node enters in the scene tree the first time
+func _ready():
+	initial_position = position
 
-func is_moving() -> bool:
-	return self.direction.x != 0 or self.direction.y != 0
 
-func _ready() -> void:
-	self.pixels_per_second = 2 * TILE_SIZE
+func _physics_process(delta):
+	if is_moving == false:
+		process_player_input()
+	elif input_direction != Vector2.ZERO:
+		move(delta)
+	else:
+		is_moving = false
 
-func _physics_process(delta: float) -> void:
-	if not is_moving(): return
-	# delta is measured in fractions of seconds, so for a speed of
-	# 4 pixels_per_second, we need to accumulate _step until 
-	# it reaches 0.25, and then we can move a pixel
-	_step += delta
-	if _step < _step_size: return
+func process_player_input():
+	if input_direction.y == 0:
+		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	if input_direction.x == 0:
+		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 
-	# Move a pixel
-	_step -= _step_size
-	_pixels_moved += 1
-	move_and_collide(direction)
+	if input_direction != Vector2.ZERO:
+		initial_position = position
+		is_moving = true
 
-	# Complete movement
-	if _pixels_moved >= TILE_SIZE:
-		direction = Vector2.ZERO
-		_pixels_moved = 0
-		_step = 0
-
-func _input(event: InputEvent) -> void:
-	if is_moving(): return
-	if Input.is_action_pressed("ui_right"):
-		direction = Vector2(1, 0)
-	elif Input.is_action_pressed("ui_left"):
-		direction = Vector2(-1, 0)
-	elif Input.is_action_pressed("ui_down"):
-		direction = Vector2(0, 1)
-	elif Input.is_action_pressed("ui_up"):
-		direction = Vector2(0, -1)
+func move(delta):
+	var desired_step: Vector2 = input_direction * TILE_SIZE / 2
+	ray.cast_to = desired_step
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		percent_moved_to_next_tile += walk_speed * delta
+		if percent_moved_to_next_tile >= 1.0:
+			position = initial_position + (TILE_SIZE * input_direction)
+			percent_moved_to_next_tile = 0.0
+			is_moving = false
+		else:
+			position = initial_position + (TILE_SIZE * input_direction * percent_moved_to_next_tile)
+	else:
+		is_moving = false
