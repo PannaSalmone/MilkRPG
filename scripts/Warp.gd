7db5a75af81dffbd9482ea0b9f10ab8ee59b385c
Panna <seanbb.3s@gmail.com>
2023-04-22 23:28:01 +0200

    CULO VA

--- scripts/Warp.gd
+++ scripts/Warp.gd
@@ -1,27 +1,20 @@
 extends Area2D
 
-@export var warp_loc= Vector2.ZERO
-var into = false
+@export var warp_to = Vector2.ZERO
 
-func _on_Warp_body_entered(body):
-	into = true 
-	print("culo")
-#if Input.is_action_just_pressed("ui_accept"):
-#		body.position = warp_loc
-		
-#func _on_body_exited(body : PhysicsBody2D):
-#	into = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
 
-
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if into == true:
-		if Input.is_action_just_pressed("ui_accept"):
-			print("culo")
-		#global_position = warp_loc
+	pass
 
-
-
-
-
+func _on_body_entered(body):
+	body.position = warp_to
+	
+	
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 300
-var culo = false
-
-func _on_body_entered(body):	 
-	culo = true
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
-	if culo == true:
-		if Input.is_action_just_pressed("ui_accept"):
-			print("culo")
 	get_input()
 	move_and_slide()
 
