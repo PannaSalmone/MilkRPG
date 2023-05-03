1719c1bd257c1551c922d8e74cd4b2c9e8cabb66
Panna <seanbb.3s@gmail.com>
2023-05-03 16:34:42 +0200

    lago dot

--- scripts/TelepMap.gd
+++ scripts/TelepMap.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@export var mapindex = 0
 @export var warp_to = Vector2.ZERO
+
+var mapname = Global.IDmap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,8 +16,7 @@ func _ready():
 func _process(delta):
 	pass
 
-func _on_body_entered(body):
+func _on_body_entered(body):	
+	get_tree().change_scene_to_file("res://"+ (mapname[mapindex]) +".tscn")
 	body.position = warp_to
-	print ("warp to", warp_to)
-	
-
+	print("warp to ", mapname[mapindex], " at ", warp_to) 
